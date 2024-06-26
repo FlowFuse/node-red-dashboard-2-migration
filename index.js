b6dd0b99fc4814b83a6332184c93cf26426cc1b8
Joe Pavitt <joepavitt@flowforge.com>
2024-06-26 15:59:26 +0100

    Improve robustness of checks against id by creating our own

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const generators = require('./generators')
+const utils = require('./utils')
 
 const transformerMap = require('./transformers/map.json')
 const transformers = require('./transformers')
 
 const MigrateDashboard = {
     migrate: function (flow) {
+        const idMap = {} // map old IDs to new IDs so that we can update references to them
+
         const migratedFlow = []
 
         // generate nodes we know D1.0 doesn't provide
@@ -16,12 +19,18 @@ const MigrateDashboard = {
 
         // loop over all nodes in the flow.json
         flow.forEach(node => {
+            const oldId = node.id
+            const newId = utils.generateId()
+            node.id = newId
+            idMap[oldId] = newId
+
             if (
                 Object.prototype.hasOwnProperty.call(transformerMap, node.type) &&
                 Object.prototype.hasOwnProperty.call(transformers, transformerMap[node.type]) &&
                 typeof transformers[transformerMap[node.type]] === 'function'
             ) {
-                migratedFlow.push(transformers[transformerMap[node.type]](node, base.id, theme.id))
+                const migratedNode = transformers[transformerMap[node.type]](node, base.id, theme.id)
+                migratedFlow.push(migratedNode)
                 return
             }
 
@@ -34,6 +43,25 @@ const MigrateDashboard = {
             // We don't have any particular migration logic for this node type
             // Not a Dashboard 1.0 node, we can just return it as is
             migratedFlow.push(node)
+        })
+
+        console.log(idMap)
+        // loop ovr all nodes in thw flow and ensure we have updated references:
+
+        migratedFlow.forEach(node => {
+            if (node.wires && node.wires.length) {
+                node.wires = node.wires.map(wire => {
+                    return wire.map(id => {
+                        return idMap[id] || id
+                    })
+                })
+            }
+            if (node.group) {
+                node.group = idMap[node.group] || node.group
+            }
+            if (node.page) {
+                node.page = idMap[node.page] || node.page
+            }
         })
 
         return migratedFlow
