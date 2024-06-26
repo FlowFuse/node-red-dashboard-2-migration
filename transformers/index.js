c287b1b5deedc3fbae973245d4a41be4b05423e0
Joe Pavitt <joepavitt@flowforge.com>
2024-06-26 16:21:28 +0100

    UI Form: Add support for auto migration of ui_form

--- transformers/index.js
+++ transformers/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     uiPage: require('./nodes/ui-page'),
     uiGroup: require('./nodes/ui-group'),
+    uiForm: require('./nodes/ui-form'),
     uiText: require('./nodes/ui-text')
 }
