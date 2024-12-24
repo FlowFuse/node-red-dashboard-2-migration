796185a79fb736ff3641aa886db240fdddda1e62
Connor KD9LSV <connor@kd9lsv.me>
2024-12-24 14:39:31 -0600

    Lint Cleaning

--- transformers/nodes/ui-notification.js
+++ transformers/nodes/ui-notification.js
@@ -2,30 +2,30 @@ module.exports = function (node, baseId, themeId) {
     node.type = 'ui-notification'
 
     // update properties
-    if (node.position=="dialog"|| node.position == "prompt"){
-    node.position="center center"
-    node.allowConfirm=true
-    node.allowDismiss=true
-    node.confirmText=node.ok
-    node.dismissText=node.cancel
-    }else{
-    node.allowConfirm=false
-    node.allowDismiss=false
-    node.confirmText=node.ok
-    node.dismissText=node.cancel
+    if (node.position === 'dialog' || node.position === 'prompt') {
+        node.position = 'center center'
+        node.allowConfirm = true
+        node.allowDismiss = true
+        node.confirmText = node.ok
+        node.dismissText = node.cancel
+    } else {
+        node.allowConfirm = false
+        node.allowDismiss = false
+        node.confirmText = node.ok
+        node.dismissText = node.cancel
     }
-    if (node.highlight!=""){
-        node.colorDefault=false
-        node.color=node.highlight;   
-    }else{
-        node.colorDefault=true;
-        node.color=null
+    if (node.highlight !== '') {
+        node.colorDefault = false
+        node.color = node.highlight
+    } else {
+        node.colorDefault = true
+        node.color = null
     }
-   
+
     // new properties
-  
-    node.ui=baseId;
-    node.showCountdown=false
+
+    node.ui = baseId
+    node.showCountdown = false
 
     // remove properties
     // NONE
